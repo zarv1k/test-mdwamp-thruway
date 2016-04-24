26e2fdcc433597e982585ff7465872c874c25db6
Dmitry Zarva <zarv1k@gmail.com>
2016-04-24 12:49:21 +0300

    Initial Commit

---
+++ Test MDWamp with Thruway Router/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test MDWamp with Thruway Router
+//
+//  Created by Dmitry Zarva on 24/04/16.
+//  Copyright © 2016 Dmitry Zarva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
